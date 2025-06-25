b2b673a8a32b6994e8958086092fe8fed4bee8a3
muhfachriannoor <muhammadfachriannoor@gmail.com>
2025-06-25 15:33:54 +0800

    add: add view for testLogic

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,18 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function viewTestLogic()
+    {
+        for($i = 1; $i <= 30; $i++) {
+            if($i % 14 == 0 && $i % 4 == 0) {
+                echo 'Unictive Media' . '<br>';
+            } elseif($i % 4 == 0) {
+                echo 'Unictive' . '<br>';
+            } else {
+                echo $i . '<br>';
+            }
+        }
+        // echo 'test';
+    }
 }
